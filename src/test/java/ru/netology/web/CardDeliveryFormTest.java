797dd8bbcab7e7e435df75d61557ec2d97a17d29
Natalya Pletneva <nntalita@gmail.com>
2022-01-12 22:43:35 +0300

    add date

--- src/test/java/ru/netology/web/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/web/CardDeliveryFormTest.java
@@ -1,20 +1,37 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryFormTest {
 
     @Test
-    public void shouldFormComplete() {
-        Selenide.open("http://localhost:9999/");
-        $("[placeholder='Город']").setValue("Нижний Новгород");
-        $("[class='menu-item menu-item_type_block menu-item_theme_alfa-on-white']").click();
+    public void shouldFormTest() {
+        open("http://localhost:9999");
+        $("[placeholder='Город']").setValue("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        Calendar calendar = new GregorianCalendar();
+        calendar.roll(Calendar.DAY_OF_MONTH, 3);
+        Date date = calendar.getTime();
+        SimpleDateFormat formater = new SimpleDateFormat("ddMMyyyy");
+        $("[data-test-id=date] input").setValue(formater.format(date));
         $("[data-test-id=name] input").setValue("Иванов Петр");
         $("[data-test-id=phone] input").setValue("+79027896541");
         $(".checkbox__box").click();
